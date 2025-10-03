c630b1e01ed764e0f8a269a3c648f95e807d5b9c
miaraylight <miaraylight@gmail.com>
2025-10-03 15:43:15 -0500

    implement listing All Vehicles

--- src/main/java/com/pluralsight/Inventory.java
+++ src/main/java/com/pluralsight/Inventory.java
@@ -1,7 +1,10 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Inventory {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Vehicle[] vehicles = new Vehicle[20]; // array for up to 20 vehicles
         int vehicleCount = 0; // counter for number of vehicles stored
 
@@ -13,6 +16,34 @@ public class Inventory {
         vehicles[vehicleCount++] = new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500f);
         vehicles[vehicleCount++] = new Vehicle(101126, "Jeep Wrangler", "Yellow", 30000, 16000f);
 
+
+
+        System.out.println("What do you want to do?");
+        System.out.println(" 1 - List all vehicles");
+        System.out.println(" 2 - Search by make/model");
+        System.out.println(" 3 - Search by price range");
+        System.out.println(" 4 - Search by color");
+        System.out.println(" 5 - Add a vehicle");
+        System.out.println(" 6 - Quit");
+        System.out.println("Enter your command: ");
+
+        int command = scanner.nextInt();
+        switch(command) {
+            case 1:
+                listAllVehicles(vehicles, vehicleCount);
+                break;
+            case 2:
+                //findVehiclesByPrice();
+                break;
+            case 5:
+                //addAVehicle();
+                break;
+            case 6:
+                return;
+        }
+    }
+
+    public static void listAllVehicles (Vehicle[] vehicles, int vehicleCount) {
         // Print the vehicle list
         System.out.println("Preloaded Vehicles in Inventory:\n");
 
